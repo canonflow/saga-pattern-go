edf804614ca307dff0f9f5678c861b5adad7aadf
Garzya Santoso, Nathan <Nathan.GarzyaSantoso@contracted.sampoerna.com>
2026-08-06 16:30:57 +0700

    feat(orchestration): setup api entry point

--- orchestration/cmd/api/main.go
+++ orchestration/cmd/api/main.go
@@ -1,10 +1,43 @@
 package main
 
-import "github.com/joho/godotenv"
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+// Start Payment and Shipping Process (Simulate the distributed system)
+func startDistributed()
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		panic(err)
 	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go startDistributed()
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	// Shutdown server
+	_, cancelServer := context.WithTimeout(ctx, 5*time.Second)
+	defer cancelServer()
+
+	// if err := server.Shutdown(ctx); err != nil {
+	// 	cancelQueue()
+	// 	logrus.Infoln("Server Shutdown:", err)
+	// }
+	// logrus.Infoln("Server exiting")
 }
